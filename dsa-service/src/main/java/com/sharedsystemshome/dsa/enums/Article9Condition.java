4ba857a37d4d65d7d3756ac224a2e1be0dd2d308
John Arnett <john.arnett@sharedsystemshome.com>
2025-05-31 12:40:22 +0100

    Updated to add Article9Condition values DataContentPerspective instantiation.

--- dsa-service/src/main/java/com/sharedsystemshome/dsa/enums/Article9Condition.java
+++ dsa-service/src/main/java/com/sharedsystemshome/dsa/enums/Article9Condition.java
@@ -2,12 +2,26 @@ package com.sharedsystemshome.dsa.enums;
 
 public enum Article9Condition {
 
-    EXPLICIT_CONSENT,
-    VITAL_INTERESTS,
-    LEGAL,
-    REASONS_OF_PUBLIC_INTEREST,
-    HEALTH_OR_SOCIAL_CARE,
-    PUBLIC_HEALTH,
-    ARCHIVING;
+    NOT_APPLICABLE("N/A"),
+    EXPLICIT_CONSENT("Explicit consent"),
+    EMPLOYMENT("Employment, Social Security, or Social Protection Law"),
+    VITAL_INTERESTS("Vital Interests"),
+    NOT_FOR_PROFIT("Not-for-Profit Bodies"),
+    DATA_MADE_PUBLIC("Data Made Public by the Data Subject"),
+    LEGAL("Legal Claims or Judicial Acts"),
+    REASONS_OF_PUBLIC_INTEREST("Substantial Public Interest (UK law)"),
+    HEALTH_OR_OCCUP_CARE("Healthcare and Occupational Medicine"),
+    PUBLIC_HEALTH("Public Health"),
+    ARCHIVING("Archiving, Research, or Statistical Purposes");
+
+    private final String article9Condition;
+
+    Article9Condition(String article9Condition) {
+        this.article9Condition = article9Condition;
+    }
+
+    public String getLabel() {
+        return article9Condition;
+    }
 
 }
