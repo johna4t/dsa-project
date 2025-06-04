ff160c5e3bdce3f453ad430e580a0f237f271c8f
John Arnett <john.arnett@sharedsystemshome.com>
2025-06-04 08:51:10 +0100

    Updated to add GDPR Perspective data

--- dsa-client/src/app/data-content-definition/create-data-content-definition/create-data-content-definition.component.ts
+++ dsa-client/src/app/data-content-definition/create-data-content-definition/create-data-content-definition.component.ts
@@ -1,9 +1,18 @@
+// ✅ Updated: Use DataContentType enum and labels
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataContentDefinitionService } from '../data-content-definition.service';
 import { DataContentDefinition } from '../data-content-definition';
 import { UserLocalStorageService } from '../../access/user-local-storage.service';
+import { LawfulBasis } from '../../enums/lawful-basis.enum';
+import { LawfulBasisLabels } from '../../enums/lawful-basis-labels';
+import { SpecialCategoryData } from '../../enums/special-category-data.enum';
+import { SpecialCategoryDataLabels } from '../../enums/special-category-data-labels';
+import { Article9Condition } from '../../enums/article-9-condition.enum';
+import { Article9ConditionLabels } from '../../enums/article-9-condition-labels';
+import { DataContentType } from '../../enums/data-content-type.enum';
+import { DataContentTypeLabels } from '../../enums/data-content-type-labels';
 
 @Component({
   selector: 'app-create-data-content-definition',
@@ -11,13 +20,18 @@ import { UserLocalStorageService } from '../../access/user-local-storage.service
 })
 export class CreateDataContentDefinitionComponent implements OnInit {
   dcdForm!: FormGroup;
-  contentTypes = [
-    'NOT_SPECIFIED',
-    'STRUCTURED_ELECTR0NIC_DATA',
-    'ELECTRONIC_DOCUMENT',
-    'PAPER_DOCUMENT',
-    'OTHER_MEDIUM',
-  ];
+
+  dataContentTypeKeys = Object.keys(DataContentTypeLabels) as DataContentType[];
+  dataContentTypeLabels = DataContentTypeLabels;
+
+  lawfulBasisKeys = Object.keys(LawfulBasisLabels) as LawfulBasis[];
+  lawfulBasisLabels = LawfulBasisLabels;
+
+  specialCategoryKeys = Object.keys(SpecialCategoryDataLabels) as SpecialCategoryData[];
+  specialCategoryLabels = SpecialCategoryDataLabels;
+
+  article9ConditionKeys = Object.keys(Article9ConditionLabels) as Article9Condition[];
+  article9ConditionLabels = Article9ConditionLabels;
 
   constructor(
     private fb: FormBuilder,
@@ -34,7 +48,10 @@ export class CreateDataContentDefinitionComponent implements OnInit {
       ownerEmail: ['', [Validators.required, Validators.email]],
       retentionValue: [1, [Validators.required, Validators.min(1)]],
       retentionUnit: ['D', Validators.required],
-      dataContentType: ['NOT_SPECIFIED'],
+      dataContentType: [DataContentType.NOT_SPECIFIED],
+      lawfulBasis: [LawfulBasis.NOT_PERSONAL_DATA],
+      specialCategory: [SpecialCategoryData.NOT_SPECIAL_CATEGORY_DATA],
+      article9Condition: [Article9Condition.NOT_APPLICABLE],
     });
   }
 
@@ -43,7 +60,6 @@ export class CreateDataContentDefinitionComponent implements OnInit {
 
     const formValues = this.dcdForm.value;
     const retentionPeriod = `P${formValues.retentionValue}${formValues.retentionUnit}`;
-
     const customerAccountId = this.userLocalStorageService.getUserParentAccountId();
     if (customerAccountId === null) {
       console.error('Customer account ID not found in local storage');
@@ -54,6 +70,16 @@ export class CreateDataContentDefinitionComponent implements OnInit {
       ...formValues,
       retentionPeriod,
       provider: { id: customerAccountId, name: '' },
+      perspectives: [
+        {
+          metadataScheme: 'GDPR',
+          metadata: {
+            lawfulBasis: formValues.lawfulBasis,
+            specialCategory: formValues.specialCategory,
+            article9Condition: formValues.article9Condition,
+          },
+        },
+      ]
     };
 
     this.dcdService.postDataContentDefinition(newDcd).subscribe(() => {
