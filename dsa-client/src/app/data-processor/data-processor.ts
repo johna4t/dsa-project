7123104de2808b65f6c61124c6032920521fd00f
John Arnett <john.arnett@sharedsystemshome.com>
2025-06-19 08:56:22 +0100

    Added data object members

--- dsa-client/src/app/data-processor/data-processor.ts
+++ dsa-client/src/app/data-processor/data-processor.ts
@@ -1,1 +1,13 @@
-export class DataProcessor { }
+import { DataSharingParty } from '../data-sharing-party/data-sharing-party';
+import { ProcessingCertificationStandard } from '../enums/processing-certification-standard.enum';
+export class DataProcessor {
+  id = 0;
+  controller!: DataSharingParty;
+  name = '';
+  description?: string;
+  certifications: ProcessingCertificationStandard[] = [];;
+  email = '';
+  website?: string;
+  isReferenced = false;
+
+}
