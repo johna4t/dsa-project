8657c0d2d6b2106c1b569c40c95969d8779b87ea
John Arnett <john.arnett@sharedsystemshome.com>
2025-06-19 09:13:07 +0100

    Added service methods.

--- dsa-client/src/app/data-processor/data-processor.service.ts
+++ dsa-client/src/app/data-processor/data-processor.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataProcessor } from './data-processor';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataProcessorService {
 
-  constructor() { }
+    private baseUrl = 'http://localhost:8080/api/v1/data-processors';
+
+    constructor(private httpClient: HttpClient) { }
+
+    getDataProcessorList(): Observable<DataProcessor[]> {
+      return this.httpClient.get<DataProcessor[]>(this.baseUrl)
+    }
+
+    postDataProcessor(dp: DataProcessor): Observable<Object> {
+      return this.httpClient.post(this.baseUrl,dp);
+    }
+
+    getDataProcessorById(id: number): Observable<DataProcessor> {
+      return this.httpClient.get<DataProcessor>(this.baseUrl + "/" + id);
+    }
+
+    putDataProcessor(id: number, dp: DataProcessor): Observable<DataProcessor> {
+      return this.httpClient.put<DataProcessor>(this.baseUrl + "/" + id, dp);
+    }
+
+    deleteDataProcessor(id: number): Observable<DataProcessor> {
+
+      return this.httpClient.delete<DataProcessor>(this.baseUrl + "/" + id);
+    }
 }
