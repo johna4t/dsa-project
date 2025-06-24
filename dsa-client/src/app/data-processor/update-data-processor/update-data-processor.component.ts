186fecc60bb47940e3a88e9c2ba7d5540f2dff11
John Arnett <john.arnett@sharedsystemshome.com>
2025-06-24 21:02:00 +0100

    Updated so that any change to main form or certifications dirties form and enables submit button.

--- dsa-client/src/app/data-processor/update-data-processor/update-data-processor.component.ts
+++ dsa-client/src/app/data-processor/update-data-processor/update-data-processor.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DataProcessor } from '../data-processor';
 import { DataProcessorService } from '../data-processor.service';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { DataProcessor } from '../data-processor';
 import { ProcessingCertificationStandard } from '../../enums/processing-certification-standard.enum';
 import { ProcessingCertificationStandardLabels } from '../../enums/processing-certification-standard-labels';
 
@@ -12,82 +12,106 @@ import { ProcessingCertificationStandardLabels } from '../../enums/processing-ce
   styleUrls: ['./update-data-processor.component.css'],
 })
 export class UpdateDataProcessorComponent implements OnInit {
-  dpForm!: FormGroup;
   id = 0;
-  dp?: DataProcessor;
+  dpForm!: FormGroup;
+  dp!: DataProcessor;
+  originalFormValue: any;
 
-  processingCertificationStandard = ProcessingCertificationStandard;
   processingCertificationStandardLabels = ProcessingCertificationStandardLabels;
 
-  infoSecStandards = [
+  // Certification groupings
+  infoSecStandards: ProcessingCertificationStandard[] = [
     ProcessingCertificationStandard.ISO_IEC_27001,
     ProcessingCertificationStandard.ISO_IEC_27002,
     ProcessingCertificationStandard.CYBER_ESSENTIALS,
     ProcessingCertificationStandard.NIST_SP,
   ];
-  privacyStandards = [
+
+  privacyStandards: ProcessingCertificationStandard[] = [
     ProcessingCertificationStandard.ISO_IEC_27701,
     ProcessingCertificationStandard.ISO_IEC_27018,
     ProcessingCertificationStandard.NIST_PRIVACY_FRAMEWORK,
   ];
-  bcdrStandards = [
+
+  bcdrStandards: ProcessingCertificationStandard[] = [
     ProcessingCertificationStandard.ISO_IEC_22301,
     ProcessingCertificationStandard.ISO_IEC_27031,
   ];
-  governanceStandards = [
+
+  governanceStandards: ProcessingCertificationStandard[] = [
     ProcessingCertificationStandard.ISO_IEC_20000_1,
     ProcessingCertificationStandard.COBIT,
   ];
 
-  originalFormValue: any;
-
   constructor(
-    private fb: FormBuilder,
     private route: ActivatedRoute,
     private dpService: DataProcessorService,
+    private fb: FormBuilder,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-
     this.dpService.getDataProcessorById(this.id).subscribe((response) => {
       this.dp = response;
-
-      this.dpForm = this.fb.group({
-        name: [response.name],
-        description: [response.description],
-        website: [response.website],
-        certifications: [response.certifications],
-      });
-
+      this.initForm(response);
       this.originalFormValue = this.dpForm.getRawValue();
     });
   }
 
-  isCertificationChecked(cert: ProcessingCertificationStandard): boolean {
-    return this.dpForm.value.certifications.includes(cert);
+  initForm(dp: DataProcessor): void {
+    const allCerts = this.getAllCerts();
+
+    this.dpForm = this.fb.group({
+      name: [dp.name, Validators.required],
+      description: [dp.description],
+      website: [dp.website],
+      certifications: this.fb.array(
+        allCerts.map(
+          (cert) => new FormControl(dp.certifications.includes(cert))
+        )
+      ),
+    });
   }
 
-  onCertChange(cert: ProcessingCertificationStandard): void {
-    const currentCerts = this.dpForm.value.certifications || [];
-    const updated = currentCerts.includes(cert)
-      ? currentCerts.filter((c: ProcessingCertificationStandard) => c !== cert)
-      : [...currentCerts, cert];
+  get certificationsArray(): FormArray {
+    return this.dpForm.get('certifications') as FormArray;
+  }
 
-    this.dpForm.patchValue({ certifications: updated });
+  getAllCerts(): ProcessingCertificationStandard[] {
+    return [
+      ...this.infoSecStandards,
+      ...this.privacyStandards,
+      ...this.bcdrStandards,
+      ...this.governanceStandards,
+    ];
   }
 
   onSubmit(): void {
-    if (this.dpForm.valid) {
-      const updated = { ...this.dp, ...this.dpForm.value };
-      this.dpService.putDataProcessor(this.id, updated).subscribe(() => {
-        this.router.navigate(['/data-processors']);
-      });
-    }
+    const selectedCerts = this.getAllCerts().filter(
+      (_, i) => this.certificationsArray.at(i).value
+    );
+
+    const updated: DataProcessor = {
+      ...this.dp,
+      ...this.dpForm.value,
+      certifications: selectedCerts,
+    };
+
+    this.dpService.putDataProcessor(this.id, updated).subscribe(() => {
+      this.router.navigate(['/data-processors']);
+    });
   }
 
   shouldDisableSubmit(): boolean {
-    return !this.dpForm || !this.dpForm.dirty || JSON.stringify(this.dpForm.getRawValue()) === JSON.stringify(this.originalFormValue);
+    return !this.dpForm.dirty || this.dpForm.pristine;
   }
+
+  getCertIndex(cert: ProcessingCertificationStandard): number {
+  return this.getAllCerts().indexOf(cert);
+}
+
+getCertControl(index: number): FormControl {
+  return this.certificationsArray.at(index) as FormControl;
+}
 }
