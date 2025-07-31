7d6e0805826f590b55ce4b85f3f7ed0296e7a270
John Arnett <john.arnett@sharedsystemshome.com>
2025-07-31 09:07:08 +0100

    Completed and successfully tested DataProcessingActivityController.

--- dsa-service/src/main/java/com/sharedsystemshome/dsa/controller/DataProcessingActivityController.java
+++ dsa-service/src/main/java/com/sharedsystemshome/dsa/controller/DataProcessingActivityController.java
@@ -1,10 +1,9 @@
 package com.sharedsystemshome.dsa.controller;
 
 import com.sharedsystemshome.dsa.model.DataProcessingActivity;
-import com.sharedsystemshome.dsa.model.DataProcessor;
 import com.sharedsystemshome.dsa.security.service.UserContextService;
+import com.sharedsystemshome.dsa.security.util.SecurityValidationException;
 import com.sharedsystemshome.dsa.service.DataProcessingActivityService;
-import com.sharedsystemshome.dsa.service.DataProcessorService;
 import com.sharedsystemshome.dsa.util.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
@@ -32,6 +31,19 @@ public class DataProcessingActivityController {
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping
+    public ResponseEntity<Long> postDataProcessingActivity(RequestEntity<DataProcessingActivity> request){
+
+        try{
+            return ResponseEntity.status(201).body(
+                    this.dpvService.createDataProcessingActivity(
+                            this.userContext.validateAccess(request.getBody())));
+        } catch(Exception e) {
+            throw new BadRequestException(e.getMessage(), e);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "{id}")
     public ResponseEntity<DataProcessingActivity> getDataProcessingActivityById(@PathVariable("id") Long id) {
 
@@ -43,6 +55,79 @@ public class DataProcessingActivityController {
         } catch (EntityNotFoundException e) {
             return ResponseEntity.status(204).build();
         }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping
+    public ResponseEntity<List<DataProcessingActivity>> getDataProcessingActivities(RequestEntity<Map<String, String>> requestEntity) {
+        Map<String, String> queryParams = requestEntity.getBody();
+        List<DataProcessingActivity> dpvs = null;
+
+        if(null == queryParams || queryParams.isEmpty()) {
+
+            // TODO: SUPER_ADMIN returns empty for now — behavior to be enhanced later
+            dpvs = this.dpvService.getDataProcessingActivities(this.userContext.getCurrentCustomerAccountId());
+
+            if(null == dpvs || dpvs.isEmpty()) {
+
+                return ResponseEntity.status(204).build();
+
+            }
+
+            return ResponseEntity.status(200).body(dpvs);
+
+        } else {
+            throw new BadRequestException("Unrecognised query parameter.");
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PutMapping(path = "{id}")
+    public ResponseEntity<DataProcessingActivity> putDataProcessingActivity(
+            @PathVariable("id") Long id,
+            RequestEntity<DataProcessingActivity> request){
+
+        try {
+            // Access Validation
+            this.userContext.validateAccess(this.dpvService.getDataProcessingActivityById(id));
+
+            // Apply Update
+            this.dpvService.updateDataProcessingActivity(request.getBody());
+
+            // Return 204 No Content for successful update without response body
+            return ResponseEntity.status(204).build();
+
+        } catch (EntityNotFoundException | SecurityValidationException e) {
+            // Silent fail for security/privacy
+            return ResponseEntity.status(204).build();
+        } catch (Exception e) {
+            // Explicit client error for malformed or invalid payloads
+            throw new BadRequestException(e.getMessage(), e);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<DataProcessingActivity> deleteDataProcessingActivity(@PathVariable("id") Long id){
+
+        try {
+            // Access Validation
+            this.userContext.validateAccess(this.dpvService.getDataProcessingActivityById(id));
+
+            // Delete record
+            this.dpvService.deleteDataProcessingActivity(id);
+
+            // Return 204 No Content for successful update without response body
+            return ResponseEntity.status(204).build();
+
+        } catch (EntityNotFoundException | SecurityValidationException e) {
+            // Silent fail for security/privacy
+            return ResponseEntity.status(204).build();
+        } catch (Exception e) {
+            // Explicit client error for malformed or invalid payloads
+            throw new BadRequestException(e.getMessage(), e);
+        }
+
     }
 
 }
